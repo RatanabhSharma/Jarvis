a4eac96f3834114191d415ea1666d28185d84055
Ratanabh Sharma <71549354+RatanabhSharma@users.noreply.github.com>
2023-03-09 12:44:04 +0530

    Update Play_songs.py

--- Ai_package/Features/Play_songs.py
+++ Ai_package/Features/Play_songs.py
@@ -5,7 +5,7 @@ from random import randint
 def play_songs():
     n = random.randint(0,2)
     print(n)
-    music_dir = 'E:\Ratanabh\Ratanabh\Vlogs\Music'
+    music_dir = 'Put your music directory link here'
     songs = os.listdir(music_dir)
     print(songs)
     os.startfile(os.path.join(music_dir,songs[n]))
